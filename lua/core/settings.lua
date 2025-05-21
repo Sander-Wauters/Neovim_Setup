0a5544b18d14ebf839948659ae49a90e92a9f5fc
Sander-Wauters <sander1352@hotmail.be>
2025-05-21 18:12:20 +0200

    Refactor settings

--- lua/core/settings.lua
+++ lua/core/settings.lua
@@ -1,39 +1,41 @@
+local o = vim.opt
+
 -- line numbers
-vim.opt.number = true
-vim.opt.relativenumber = true
+o.number = true
+o.relativenumber = true
 
 -- tabs & indentation
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-vim.opt.smartindent = true
+o.tabstop = 4
+o.softtabstop = 4
+o.shiftwidth = 4
+o.expandtab = true
+o.smartindent = true
 
 -- search settings
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
+o.hlsearch = false
+o.incsearch = true
+o.ignorecase = true
+o.smartcase = true
 
 -- line wrapping
-vim.opt.wrap = false
+o.wrap = false
 
 -- swap files
-vim.opt.swapfile = false
-vim.opt.backup = false
+o.swapfile = false
+o.backup = false
 
 -- cursorline
-vim.opt.cursorline = true
+o.cursorline = true
 
 -- appearance
-vim.opt.termguicolors = true
+o.termguicolors = true
 
 -- amount of lines shown at top of screen
-vim.opt.scrolloff = 8
+o.scrolloff = 8
 
 -- copy clipboard
-vim.opt.clipboard:append("unnamedplus")
+o.clipboard = "unnamedplus"
 
 -- split windows
-vim.opt.splitright = true
-vim.opt.splitbelow = true
+o.splitright = true
+o.splitbelow = true
