e652e7a7e1f08316980d9fc7eb11dba9087fad34
Sander-Wauters <sander1352@hotmail.be>
2025-05-21 18:47:22 +0200

    Add keymappings to lsp

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -19,31 +19,40 @@ end
 -- keybinds for available lsp server
 local onAttach = function(client, bufnr)
     local opts = { noremap = true, silent = true, buffer = bufnr }
+    local map = vim.keymap.set
+
+    map("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename" })
+    map("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Get code actions" })
+
+    map("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+    map("n", "gi", vim.lsp.buf.implementation, { desc = "Go to implementation" })
+    map("n", "gr", require("telescope.builtin").lsp_references, { desc = "Go to references" })
+    map("n", "K", vim.lsp.buf.hover, { desc = "Show documentation" })
 end
 
 -- used to enable autocompletion
 local capabilities = cmpNvimLsp.default_capabilities()
 
 -- cpp
-lspconfig.clangd.setup{
+lspconfig.clangd.setup {
     capabilities = capabilities,
     on_attach = onAttach,
 }
 
 -- css
-lspconfig.cssls.setup{
+lspconfig.cssls.setup {
     capabilities = capabilities,
     on_attach = onAttach,
 }
 
 -- html
-lspconfig.html.setup{
+lspconfig.html.setup {
     capabilities = capabilities,
     on_attach = onAttach,
 }
 
 -- java
-lspconfig.jdtls.setup{ 
+lspconfig.jdtls.setup {
     capabilities = capabilities,
     on_attach = onAttach,
 }
@@ -57,25 +66,25 @@ typescript.setup({
 })
 
 -- lua
-lspconfig.lua_ls.setup{
+lspconfig.lua_ls.setup {
     capabilities = capabilities,
     on_attach = onAttach,
 }
 
 -- cmake
-lspconfig.cmake.setup{
+lspconfig.cmake.setup {
     capabilities = capabilities,
     on_attach = onAttach,
 }
 
 -- python
-lspconfig.jedi_language_server.setup{
+lspconfig.jedi_language_server.setup {
     capabilities = capabilities,
     on_attach = onAttach,
 }
 
 -- gradle
-lspconfig.gradle_ls.setup{
+lspconfig.gradle_ls.setup {
     capabilities = capabilities,
     on_attach = onAttach,
 }
